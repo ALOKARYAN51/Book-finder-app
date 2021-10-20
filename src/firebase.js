8761afa78a38e4bb0ca41c85518853696df67fc9
ALOKARYAN51 <alokaryan51@gmail.com>
2021-10-21 00:53:18 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDEHYYK93FW3xyLnzl9ia3wVxLkl00n6gc",
+  authDomain: "book-finder-d67a8.firebaseapp.com",
+  projectId: "book-finder-d67a8",
+  storageBucket: "book-finder-d67a8.appspot.com",
+  messagingSenderId: "160958185797",
+  appId: "1:160958185797:web:b8d0bc813c230dc4dc7e2c"
+};
+
+const fireDb = firebase.initializeApp(firebaseConfig);
+export default fireDb.database().ref();
