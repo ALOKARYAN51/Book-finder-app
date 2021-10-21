9abe39be63b6df1b539194cd4b283f575c9ce0c2
ALOKARYAN51 <alokaryan51@gmail.com>
2021-10-21 14:27:59 +0530

    view page updated

--- src/pages/View.js
+++ src/pages/View.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import fireDb from "../firebase";
+import { useParams, Link } from "react-router-dom";
+import "./View.css";
 
 const View = () => {
-	return (
-		<div>
-			<h2>view</h2>
-		</div>
-	)
-};
+  const [user, setUser] = useState({});
 
+  const { id } = useParams();
+
+  useEffect(() => {
+    fireDb
+      .child(`date/${id}`)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          setUser({ ...snapshot.val() });
+        } else {
+          setUser({});
+        }
+      });
+  }, [id]);
+
+  console.log("user", user);
+  return (
+    <div style={{ marginTop: "150px" }}>
+      <div className="card">
+        <div className="card-header">
+          <p>User Contact Detail</p>
+        </div>
+        <div className="container">
+          <strong>ID: </strong>
+          <span>{id}</span>
+          <br />
+          <br />
+          <strong>Name: </strong>
+          <span>{user.name}</span>
+          <br />
+          <br />
+          <strong>Author: </strong>
+          <span>{user.author}</span>
+          <br />
+          <br />
+          <strong>date: </strong>
+          <span>{user.date}</span>
+          <br />
+          <br />
+          <Link to="/">
+            <button className="btn btn-edit">Go Back</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default View;
