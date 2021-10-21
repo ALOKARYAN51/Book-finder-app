d244ce627406e53dec6386fc5aca15eefc803836
ALOKARYAN51 <alokaryan51@gmail.com>
2021-10-21 22:09:41 +0530

    final update done

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -30,34 +30,48 @@ const Search = () => {
 	return (
 	<>
 		<div style={{ marginTop: "100px" }}>
-		
-        <table className="styled-table">
-        <thead>
-          <tr>
-            <th style={{ textAlign: "center" }}>No.</th>
-            <th style={{ textAlign: "center" }}>Name</th>
-            <th style={{ textAlign: "center" }}>Author</th>
-            <th style={{ textAlign: "center" }}>Date</th>
+			<Link to = "/">
+			<button className="btn btn-edit"> Go Back</button>
+
+
+			</Link>
+			{Object.keys(data).length === 0 ? (
+					<h2> No search found with that name : {query.get("name")}</h2>
+
+				): (
+
+				<table className="styled-table">
+        		<thead>
+          		<tr>
+            		<th style={{ textAlign: "center" }}>No.</th>
+            		<th style={{ textAlign: "center" }}>Name</th>
+            		<th style={{ textAlign: "center" }}>Author</th>
+            		<th style={{ textAlign: "center" }}>Date</th>
             
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(data).map((id, index) => {
-            return (
-              <tr key={id}>
-                <th scope="row">{index + 1}</th>
-                <td>{data[id].name}</td>
-                <td>{data[id].author}</td>
-                <td>{data[id].date}</td>
+          		</tr>
+        		</thead>
+        		<tbody>
+          			{Object.keys(data).map((id, index) => {
+            		return (
+              			<tr key={id}>
+                			<th scope="row">{index + 1}</th>
+                			<td>{data[id].name}</td>
+                			<td>{data[id].author}</td>
+                			<td>{data[id].date}</td>
                 
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+              			</tr>
+            		);
+          			})}
+        		</tbody>
+     			 </table>
+
+
+
+				)}
+        
+    	</div>
 	</>
-	)
+	);
 };
 
 export default Search;
